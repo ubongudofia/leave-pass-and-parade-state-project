4884dfafd3011360c996c9aeb99c588bb1444f9a
Ubong Udofia <udofiaubong10@gmail.com>
2026-05-11 07:47:23 +0100

    Use environment variables for configuration values

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
+import os
+
 class Config:
-    SECRET_KEY = 'thissecretkeyissupposedtobethelongestsecretkeyusedtoprotectyourapp'
-    # MONGO_URI = 'mongodb://localhost:27017/dsa_pass_leave'
-    MONGO_URI = 'mongodb+srv://udofiaubong10:qwerty123@dsapassleave.lyycipc.mongodb.net/?appName=dsaPassLeave'
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    MONGO_URI = os.environ.get("MONGO_URI")
