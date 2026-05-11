ec5d048d483ca67c260af410b93ba0b2232afa19
unknown <Ubong Udofia>
2026-05-11 07:22:33 +0100

    Update on Initialize_all_balances file

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 class Config:
     SECRET_KEY = 'thissecretkeyissupposedtobethelongestsecretkeyusedtoprotectyourapp'
-    MONGO_URI = 'mongodb://localhost:27017/dsa_pass_leave'
+    # MONGO_URI = 'mongodb://localhost:27017/dsa_pass_leave'
+    MONGO_URI = 'mongodb+srv://udofiaubong10:udofiaubong10@dsapassleave.lyycipc.mongodb.net/?appName=dsaPassLeave'
