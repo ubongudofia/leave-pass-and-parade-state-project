95c47e7f41ecb4d39cd1a3e6d88eff76cecefd2e
Ubong Udofia <udofiaubong10@gmail.com>
2026-05-11 07:43:01 +0100

    Change MongoDB URI password in config.py
    Updated MONGO_URI to use a new password.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY = 'thissecretkeyissupposedtobethelongestsecretkeyusedtoprotectyourapp'
     # MONGO_URI = 'mongodb://localhost:27017/dsa_pass_leave'
-    MONGO_URI = 'mongodb+srv://udofiaubong10:udofiaubong10@dsapassleave.lyycipc.mongodb.net/?appName=dsaPassLeave'
+    MONGO_URI = 'mongodb+srv://udofiaubong10:qwerty123@dsapassleave.lyycipc.mongodb.net/?appName=dsaPassLeave'
